e1457b81aacdd07ac76dbbba14bf2876c04f9a49
Szyoo <losiner.y@gmail.com>
2024-10-16 15:16:30 +0900

    修正config文件夹的ignore逻辑，修正文件夹不存在的处理

--- mcgoogledrive/config.py
+++ mcgoogledrive/config.py
@@ -6,6 +6,10 @@ CONFIG_FILE = 'config/config.json'
 
 def load_config():
     logging.info('加载配置文件')
+    # 确保 config 目录存在
+    if not os.path.exists(os.path.dirname(CONFIG_FILE)):
+        os.makedirs(os.path.dirname(CONFIG_FILE))
+    
     if os.path.exists(CONFIG_FILE):
         with open(CONFIG_FILE, 'r') as f:
             return json.load(f)
@@ -13,5 +17,9 @@ def load_config():
 
 def save_config(config):
     logging.info('保存配置中')
+    # 确保 config 目录存在
+    if not os.path.exists(os.path.dirname(CONFIG_FILE)):
+        os.makedirs(os.path.dirname(CONFIG_FILE))
+    
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f)
